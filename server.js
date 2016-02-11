5363803eefd786ccd58faa0e431f2999d6e3d420
Hack Reactor Students <accounts@hackreactor.com>
2016-02-10 16:07:45 -0800

    Added notes about stuff to server.js

--- server.js
+++ server.js
@@ -5,3 +5,36 @@ var port = 4568;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+
+//To edit live server:
+//remote add live ssh://root@178.62.239.170/root/repo/site.git
+
+// -----BEGIN RSA PRIVATE KEY-----
+// MIIEpAIBAAKCAQEAsrJbIWYqhWqkJDJzeC/PgjPD/2A022Uk8ZbDBgUMKOMdeYmC
+// 89x2TMRUdnUYgWuVAH0PEZ0jK2kRK8V+CpLXNdc0PE7Dt5e0J6VskZnIg8i62wCb
+// W68+07T5Ud8ov+frSuTU7qYsBJL0O0droFqNsqdqepoOl4srM5dOX4kLb1Jkg8zG
+// XAY33NDXlzcHocw76QgJIwytikq+8MwG4aAC+crGZis9ahu2tjum7kcZrA7n8dJd
+// kJQj4dYrCh91rVwUufe7ZbXbUkb6osuC/EZlvfIDxNgjuTaEibUBOHjz5IJVCd3S
+// +UMDb4h9NVmW/V88wzV39U3ibGIuAWG/LRQ2IQIDAQABAoIBAQCo9bbw13bw+yoE
+// B6fiReKAi42Qk1im8PWDcDcqI++hYUznM7wb86HvZ9ssvf7Tr/MMqTJsRxWP4CIa
+// LvvD55+RCtAbDaZEmy3+Jo2HBc7vvAXhOKusfwB6kC0E2mnfOdvhzof+tiP8ly5S
+// Nm9vCO/dsiodjjip1S6d8zxpXFzxfXOQC5sCQ2sNpFU6xF2Mg59YW+7QsCvqozT3
+// tiry9xwr17K/ArQjvRVQWSHvMan3gPJ7hJJwtFA26JNwsvWVsofVMPrHXbSTYFPA
+// hJLi9/Q6MeNctoBms5hoivHeNMsdeWG8o3/JmoOQhR3YkPM/bnnhXqmWpEy1RyJ1
+// iJ57NiEZAoGBANb6+C5J91/JTmCjsklkc0KqhoW1rroW2pr4F5UsqtEifFlTs7sQ
+// 3y68Oi6YHoPrMJsS+jIN0k7ZPCfunyzjaawoLYZ4ee7S8DuHRIF7aexkwGUyKjhq
+// h0XQgJeSmUCBu//0dZhavgLf4Hs5nmjTpvOe2aSnLOzQhde99Ejgyf4LAoGBANTL
+// Dv96OySEcgCAnL6h8rVCdVqwp0uouAj1EItqVoo2vWglCx2eESxH2IcZuCSNDuBK
+// MA0VJpUb7s0sFR4G9zgghdm0ylA49E8+uYSAHTb7jaiG46NRk5PHLfD+Jg89cTj9
+// n05BPeoMsPq65kl8XQ2Po1NxxAp7XVNPle6m1DQDAoGBAM9FjBF9KS/GNHZCffv7
+// CJIHPcofAIbU0BJIDty6Azu+RywM4qYCv1wdCO+xpQJFuUhaxLWL0/BHzJMno3qx
+// DEp7NmGHOPDFme+bHLbGmbrc7Q7kjgZ011W9kK4THwUt0GgYd2TcYIT++z1zsUKa
+// hkYvKuJweIUfC1yV/bhiWv4/AoGAEJDWVBFB9W/SKDPbAAQ/jtjjpfHGtF8oJJpf
+// dVsL7aqhSDBKEz4w/A+DzdC0wzOwCsn7v6j73fPUvWhHJJVKPbZadT5b2a71Zg3u
+// B2Cp5ZPjzVDi+93n7UM+UlqWv+z6mYM1GbR+i2/9fyd5shLac6nQ/zgGGQLNmyTA
+// +VlHf90CgYAWNOtC7xs6bqNMjCB97+2JN3fVLs7x1nCjBvdx6/WPtKJK1iWe5Oh3
+// h9yo/7z0Aj7GODgVuI4xFQ5VKATZtVwyS0b24d+cw2GlMixlagUrX66S53l+yDcg
+// aGvfTJab4hbGoB+7eU1o3jg05X1a4dhYtq/w0LpagaVeLiAJJv+BsQ==
+// -----END RSA PRIVATE KEY-----
+
+// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyslshZiqFaqQkMnN4L8+CM8P/YDTbZSTxlsMGBQwo4x15iYLz3HZMxFR2dRiBa5UAfQ8RnSMraRErxX4Kktc11zQ8TsO3l7QnpWyRmciDyLrbAJtbrz7TtPlR3yi/5+tK5NTupiwEkvQ7R2ugWo2yp2p6mg6Xiyszl05fiQtvUmSDzMZcBjfc0NeXNwehzDvpCAkjDK2KSr7wzAbhoAL5ysZmKz1qG7a2O6buRxmsDufx0l2QlCPh1isKH3WtXBS597tltdtSRvqiy4L8RmW98gPE2CO5NoSJtQE4ePPkglUJ3dL5QwNviH01WZb9XzzDNXf1TeJsYi4BYb8tFDYh student@HR6-08.local
