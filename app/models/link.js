033b709ee54a2facf90d7ad5c713ad6ad73cbace
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 16:21:18 -0800

    Switch out models for mongoose models

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,24 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongoose.Schema({
+  _id: Number,
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number
 });
+
+linkSchema.methods.initialize = function() {
+  this.on('creating', function(model, attrs, options) {
+    var shasum = crypto.createHash('sha1');
+    shasum.update(model.get('url'));
+    model.set('code', shasum.digest('hex').slice(0, 5));
+  });
+};
+
+var Link = mongoose.model("Link", linkSchema);
 
 module.exports = Link;
